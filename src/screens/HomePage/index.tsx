5e29b3b81953182797f0cdeaa208ab90dccafb17
Cleyton-1995 <cleytonotyelc1995@gmail.com>
2022-07-13 12:21:46 -0300

    home page completed

--- src/screens/HomePage/index.tsx
+++ src/screens/HomePage/index.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Input } from '../../components/Form/Input';
+import { AntDesign, Feather } from "@expo/vector-icons";
+import React from "react";
+import { ScrollView, View } from "react-native";
+import { ButtonHomePage } from "../../components/Form/ButtonHomePage";
+import { FooterHP } from "../../components/Form/FooterHP";
+import { Input } from "../../components/Form/Input";
+import { ProfileHeaders } from "../../components/Header/ProfileHeaders";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
-export function CustomerList() {
+export function HomePage() {
   return (
-    <View style={styles.container}>
-      
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <ProfileHeaders title="Olá, Pedro!" />
+      </View>
+      <View style={styles.content}>
+        <ButtonHomePage />
+        <AntDesign style={styles.plus} name="plus" size={24} color="#0073FB" />
+        <Feather
+          style={styles.dollar_sign}
+          name="dollar-sign"
+          size={24}
+          color="#0073FB"
+        />
+        <AntDesign
+          style={styles.addusergroup}
+          name="addusergroup"
+          size={24}
+          color="#0073FB"
+        />
+        <Feather
+          style={styles.dollar}
+          name="dollar-sign"
+          size={24}
+          color="#0073FB"
+        />
+        <FooterHP/>
+      </View>
+    </ScrollView>
   );
 }
--- src/screens/HomePage/styles.ts
+++ src/screens/HomePage/styles.ts
@@ -1,5 +1,58 @@
 import { StyleSheet } from 'react-native';
+import { COLORS } from '../../themes/colors';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    backgroundColor: COLORS.BLUE_500,
+    height: 300,
+  },
+
+  content: {
+    backgroundColor: COLORS.GRAY_QUINTENAY,
+    height: 430,
+  },
+
+  plus: {
+    bottom: 200,
+    marginLeft: 40,
+    backgroundColor: COLORS.GRAY_QUINTENAY,
+    width: 30,
+    height: 30,
+    paddingTop: 2,
+    paddingLeft: 3,
+    borderRadius : 5,
+  },
+
+  addusergroup: {
+    bottom: 160,
+    marginLeft: 180,
+    backgroundColor: COLORS.GRAY_QUINTENAY,
+    width: 30,
+    height: 30,
+    paddingTop: 2,
+    paddingLeft: 3,
+    borderRadius : 5,
+  },
+
+  dollar: {
+    bottom: 190,
+    marginLeft: 40,
+    backgroundColor: COLORS.GRAY_QUINTENAY,
+    width: 30,
+    height: 30,
+    paddingTop: 2,
+    paddingLeft: 3,
+    borderRadius : 5,
+  },
+
+  dollar_sign: {
+    bottom: 230,
+    marginLeft: 210,
+    backgroundColor: COLORS.GRAY_QUINTENAY,
+    width: 30,
+    height: 30,
+    paddingTop: 2,
+    paddingLeft: 3,
+    borderRadius : 5,
+  }
 });
