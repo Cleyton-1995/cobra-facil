c9b8e67ea3373d81f898c7307591b8945d8ab0a4
Cleyton-1995 <cleytonotyelc1995@gmail.com>
2022-09-01 21:22:20 -0300

    creation of the onboarding component

--- src/components/Form/Onboarding/index.tsx
+++ src/components/Form/Onboarding/index.tsx
@@ -1,79 +1,144 @@
 import React, { useRef, useState } from "react";
-import { Animated, Dimensions, FlatList, Text, View } from "react-native";
+import { Animated, Dimensions, FlatList, Text, View, Image, TouchableOpacity } from "react-native";
 
 import { OnboardingItem } from "../OnboardingItem";
 import { Paginator } from "../Paginator";
 import slide from "../../../../slide";
 
-interface OnboardingChargedProps {
-  viewableItems: any;
+interface OnboardingProps {
+  navigation: any;
 }
 
-const {width, height} = Dimensions.get('window');
+interface SlideProps {
+  item: any;
+}
 
-export function Onboarding() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const scrrollX = useRef(new Animated.Value(0)).current;
-  const slideRef = useRef(null);
+const { width, height } = Dimensions.get("window");
 
-  // const Footer = () => {
-  //   return (
-  //     <View style={{ height: height * 0.25, justifyContent: 'center', paddingHorizontal: 20 }}>
-  //       <View style={{ flexDirection: "row", justifyContent: 'center' }} >
-  //         <View style={[ styles.indicator,  ]} />
-  //       </View>
-  //     </View>
-  //   )
-  // }
+const Slide = ({ item }: SlideProps) => {
+  return (
+    <View style={[styles.container, { width }]}>
+      <Image
+        source={item.img}
+        style={[styles.img, { resizeMode: "contain" }]}
+      />
+      <Image
+        source={item.image}
+        style={[styles.image, { resizeMode: "contain"}]}
+      />
 
-  const viewableItemsCharged = useRef(({ viewableItems }: OnboardingChargedProps) => {
-    setCurrentIndex(viewableItems[0].index);
-  }).current;
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.paragraph}>{item.paragraph}</Text>
+    </View>
+  );
+}
 
-  // const viewConfig = useRef({ viewAreaCovaragePercentThreshold: 50 }).current;
+export function Onboarding({navigation}: OnboardingProps) {
+  
+  const [ currentSlideIndex, setCurrentSlideIndex ] = useState(0);
 
+  const Footer = () => {
+      return (
+        <View style={{ height: height * 0.25, justifyContent: 'center', paddingHorizontal: 20 }}>
+          <View style={{ flexDirection: "row", justifyContent: 'center', top: -70 }} >
+            { slide.map((_, index) => (
+            <View key={index} style={[ styles.indicator, currentSlideIndex == index && {
+              backgroundColor: COLORS.BLUE_500,
+            },
+           ]} />
+            )) }
+          </View>
+
+           <View>
+            <View>
+              <TouchableOpacity activeOpacity={0.8} >
+                <Text style={styles.btntext} >
+                  Próximo 
+                 <AntDesign name="right" size={15} color="#009BF4" style={styles.right}/>
+                 </Text>
+              </TouchableOpacity>
+            </View>
+           </View>
+
+        </View>
+      )
+    }
   return (
     <View style={styles.container}>
-      <View style={{ flex: 3 }}>
-        <FlatList
-          data={slide}
-          renderItem={({ item }) => <OnboardingItem item={item} />}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          pagingEnabled
-          bounces={false}
-          keyExtractor={(item) => item.id}
-          // onScroll={Animated.event([{ nativeEvent: { contenOffset: { x: scrrollX }}}], {
-          //     useNativeDriver: false,
-          // })}
-          scrollEventThrottle={32}
-          onViewableItemsChanged={viewableItemsCharged}
-          // viewabilityConfig={viewConfig}
-          ref={slideRef}
-        />
-      </View>
-
-          <Paginator data={slide}scrrollX />
-
+      <FlatList
+        data={slide}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => <Slide item={item}/>}
+        pagingEnabled
+      />
+      <Footer/>
     </View>
   );
 }
 
 import { StyleSheet } from "react-native";
 import { COLORS } from "../../../themes/colors";
+import { FONTS } from "../../../themes/fonts";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { AntDesign } from "@expo/vector-icons";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: getStatusBarHeight() + 17,
   },
 
   indicator: {
-    height: 20,
-    width: 45,
-    backgroundColor: COLORS.GRAY_QUATERNARY,
-    marginHorizontal: 3,
-    borderRadius: 2,
+    height: 7,
+    width: 90,
+    backgroundColor: 'grey',
+    marginHorizontal: 5,
+    borderRadius: 5,
+  },
+
+  image: {
+    flex: 0.7,
+    justifyContent: "center",
+    bottom: -10,
+  },
+
+  img: {
+    flex: 0.7,
+    justifyContent: "center",
+    width: 200,
+    top: 10,
+    position: "absolute",
+  },
+
+  title: {
+    color: COLORS.BLUE_500,
+    fontSize: 24,
+    textAlign: "center",
+    fontFamily: FONTS.BOLD,
+    width: 250,
+    top: -10,    
+  },
+
+  paragraph: {
+    color: COLORS.BLACK_PRIMARY,
+    textAlign: "center",
+    fontSize: 12,
+    width: 290,
+  },
+
+  btntext: {
+    color: COLORS.BLUE_500,
+    fontSize: 16,
+    alignSelf: "center",
+    top: -45,
+    fontFamily: FONTS.BOLD,
+  },
+
+  right: {
+    position: "absolute",
+    fontFamily: FONTS.BOLD,
   }
-})
+});
